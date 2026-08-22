3466e86cc9934da633d06254d69870be55f9ce15
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-08-22 14:39:57 +0300

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,10 +50,13 @@ android {
             isMinifyEnabled = false
         }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             if (releaseStoreFile != null) {
                 signingConfig = signingConfigs.getByName("release")
+            }
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
             }
         }
     }
