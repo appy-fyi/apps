c83a393d863e37e76ec3e55c44d885bd08f212d9
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-08-22 14:56:52 +0300

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "fyi.appy.steadygridgallery"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "fyi.appy.steadygridgallery"
         minSdk = 33
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
 
